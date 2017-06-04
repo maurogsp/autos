dbef33f4e7c708aa3d80c0681e557cee5df8779a
maurogsp <maurogaspar@gmail.com>
2017-06-04 15:38:49 -0300

    2da subida autos con spring

--- ejemplospring/src/main/java/com/maven/springboot/Controladora.java
+++ ejemplospring/src/main/java/com/maven/springboot/Controladora.java
@@ -28,21 +28,43 @@ public class Controladora {
     }
 
     @RequestMapping(value = "/autos/", method = RequestMethod.GET)
-    public @ResponseBody ResponseEntity<List<Auto>> getAutos()
-    {
-        return new ResponseEntity<List<Auto>>(servi.getautos(), HttpStatus.OK);
+    public @ResponseBody ResponseEntity<List<Auto>> getAutos() {
+        List<Auto> listaautos = servi.getautos();
+        if (listaautos.size() > 0) {
+            return new ResponseEntity<List<Auto>>(listaautos, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<List<Auto>>(HttpStatus.NO_CONTENT);
+        }
     }
 
     @RequestMapping(value = "/autos/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody ResponseEntity<Auto> getAutoxId(@PathVariable("id") String id)
     {
-        return new ResponseEntity<Auto>(servi.getautoxid(id), HttpStatus.OK);
+        Auto auto = servi.getautoxid(id);
+        if(auto!=null)
+        {
+            return new ResponseEntity<Auto>(auto, HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<Auto>(HttpStatus.NOT_FOUND);
+        }
+
     }
 
     @RequestMapping(value = "/autos", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody ResponseEntity<Auto> getByMarca(@RequestParam ("marca") String brand)
     {
-        return new ResponseEntity<Auto>(servi.getautoxmarca(brand), HttpStatus.OK);
+        Auto auto = servi.getautoxmarca(brand);
+        if(auto!=null)
+        {
+            return new ResponseEntity<Auto>(auto, HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<Auto>(HttpStatus.NOT_FOUND);
+        }
+        
     }
 
 }
